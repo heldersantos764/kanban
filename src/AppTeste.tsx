6c47e0d3e82638a677c1892d4fa1cc4e40fbeab3
heldersantos764 <heldersantos764@gmail.com>
2024-02-20 17:16:21 -0300

    Criação de página de login

---
+++ src/AppTeste.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import "./app.css";
+import Login from "./pages/Login";
+
+interface Props {}
+
+const AppTeste: FC<Props> = (props) => {
+  return (
+    <Login/>
+  );
+};
+
+export default AppTeste;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,49 @@
 import { FC } from "react";
+import "./style.css";
 
 interface Props {}
 
 const Login: FC<Props> = (props) => {
   return (
-    <>
-      Login
-    </>
+    <div className="container-login">
+      <form action="" className="form-login">
+        <h1>Login</h1>
+
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-control"
+            autoFocus
+            placeholder="Digite se e-mail"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="form-control"
+            placeholder="Digite sua senha"
+          />
+        </div>
+
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Entrar
+          </button>
+        </div>
+
+        <div>
+          <a href="#" className="link">Cadastre-se</a>
+        </div>
+      </form>
+    </div>
   );
 };
 
 export default Login;
-
